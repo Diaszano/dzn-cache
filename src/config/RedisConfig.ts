763df9e18125c128497c5792de5a9d5c68e748b6
Diaszano <luucas.dsanto@gmail.com>
2023-04-18 00:34:42 -0300

    development: Alterado a forma de connection com o redis

--- src/config/RedisConfig.ts
+++ src/config/RedisConfig.ts
@@ -1,14 +1,28 @@
+import { createPool, Factory, Pool } from 'generic-pool';
 import Redis from 'ioredis';
 import { RedisOptionsInterface } from './interface/RedisOptionsInterface';
 
 const redisOptions: RedisOptionsInterface = {
-  port: parseInt(process.env.REDIS_PORT || '6379'),
-  host: process.env.REDIS_HOST || '127.0.0.1',
-  password: process.env.REDIS_PASSWORD || undefined,
-  username: process.env.REDIS_USERNAME || undefined,
-  db: parseInt(process.env.REDIS_DATABASE || '0'),
+  port: parseInt(process.env.CACHE_REDIS_PORT || '6379'),
+  host: process.env.CACHE_REDIS_HOST || '127.0.0.1',
+  password: process.env.CACHE_REDIS_PASSWORD || undefined,
+  username: process.env.CACHE_REDIS_USERNAME || undefined,
+  db: parseInt(process.env.CACHE_REDIS_DATABASE || '0'),
+  retryStrategy: (times: number) => Math.min(times * 50, 2000),
+  maxRetriesPerRequest: 3,
 };
 
-const redisClient: Redis = new Redis(redisOptions);
+const createRedisClient = async (): Promise<Redis> => {
+  return new Redis(redisOptions);
+};
 
-export { redisClient };
+const redisFactory: Factory<Redis> = {
+  create: async (): Promise<Redis> => await createRedisClient(),
+  destroy: async (client: Redis): Promise<void> => {
+    await client.quit();
+  },
+};
+
+const redisPool: Pool<Redis> = createPool(redisFactory, { min: 1, max: 10 });
+
+export { redisPool };
