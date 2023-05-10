1242ddb0b8653fb6f895b6945e9adf27a5b6c0e0
diaszano <luucas.dsantos@gmail.com>
2023-05-09 22:51:56 -0300

    ok

--- src/cache.ts
+++ src/cache.ts
@@ -1,10 +1,61 @@
 import 'dotenv/config';
 import { RedisCacheModel } from './model/RedisCacheModel';
+import { CacheInterface } from './model/interface/CacheInterface';
 
-if (process.env.CACHE_DRIVER == 'redis') {
-  const Cache = new RedisCacheModel();
-} else {
-  const Cache = null;
+class Cache implements CacheInterface {
+  private service: RedisCacheModel;
+
+  constructor() {
+    this.service = new RedisCacheModel();
+  }
+
+  public async flush(): Promise<void> {
+    return this.service.flush();
+  }
+
+  public async get(key: string): Promise<string | null> {
+    return this.service.get(key);
+  }
+
+  public async has(key: string): Promise<boolean> {
+    return this.service.has(key);
+  }
+
+  public async pull(key: string): Promise<string | null> {
+    return this.service.pull(key);
+  }
+
+  public async forget(key: string): Promise<void> {
+    return this.service.forget(key);
+  }
+
+  public async set(key: string, value: string): Promise<void> {
+    return this.service.set(key, value);
+  }
+
+  public async put(
+    key: string,
+    value: string,
+    seconds?: number,
+  ): Promise<void> {
+    return this.service.put(key, value, seconds);
+  }
+
+  public async remember(
+    key: string,
+    value: string,
+    seconds?: number,
+  ): Promise<string> {
+    return this.service.remember(key, value, seconds);
+  }
+
+  public async rememberForever(key: string, value: string): Promise<string> {
+    return this.service.rememberForever(key, value);
+  }
+
+  public async forever(key: string, value: string): Promise<void> {
+    return this.service.forever(key, value);
+  }
 }
 
-export { Cache };
+export default new Cache();
