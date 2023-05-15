81cf1810ded6b23ab8290f94bc4e9d95a7df622a
diaszano <luucas.dsantos@gmail.com>
2023-05-15 17:56:58 -0300

    refactor/classCache: Movido o Cache.redis.ts e refatorado, colocando a criação da conexão com o Redis dentro da classe CacheRedis.

--- src/Model/Redis/Cache.redis.ts
+++ src/Model/Redis/Cache.redis.ts
@@ -1,10 +1,22 @@
+import RedisOptions, { RedisOptionsDefault } from './Interface/RedisOptions';
 import Redis, { ChainableCommander } from 'ioredis';
-import { CacheInterface } from './interface/CacheInterface';
-import { Pool } from 'generic-pool';
-import { redisPool as redisConnection } from '../config/RedisConfig';
+import { createPool, Factory, Pool } from 'generic-pool';
+import { Cache as CacheInterface } from '../Interface/Cache';
 
-class RedisCacheModel implements CacheInterface {
-  constructor(private readonly redisPool: Pool<Redis> = redisConnection) {}
+export class CacheRedis implements CacheInterface {
+  private readonly redisFactory: Factory<Redis> = {
+    create: async (): Promise<Redis> => await this.createRedisClient(),
+    destroy: async (client: Redis): Promise<void> => {
+      await client.quit();
+    },
+  };
+
+  private readonly redisPool: Pool<Redis> = createPool(this.redisFactory, {
+    min: 1,
+    max: 10,
+  });
+
+  constructor(private readonly options: RedisOptions = RedisOptionsDefault) {}
 
   public async flush(): Promise<void> {
     await this.redisPool.use((client: Redis) => client.flushall());
@@ -36,14 +48,6 @@ class RedisCacheModel implements CacheInterface {
     value: string | (() => string),
     seconds?: number,
   ): Promise<void> {
-    await this.put(key, value, seconds);
-  }
-
-  public async put(
-    key: string,
-    value: string | (() => string),
-    seconds?: number,
-  ): Promise<void> {
     const redisClient: Redis = await this.redisPool.acquire();
     const pipeline: ChainableCommander = redisClient.pipeline();
 
@@ -65,11 +69,22 @@ class RedisCacheModel implements CacheInterface {
     await this.redisPool.release(redisClient);
   }
 
+  public async add(
+    key: string,
+    value: string | (() => string),
+    seconds?: number,
+  ): Promise<void> {
+    if (await this.has(key)) {
+      return;
+    }
+    await this.set(key, value, seconds);
+  }
+
   public async forever(
     key: string,
     value: string | (() => string),
   ): Promise<void> {
-    await this.put(key, value);
+    await this.set(key, value);
   }
 
   public async rememberForever(
@@ -98,10 +113,12 @@ class RedisCacheModel implements CacheInterface {
       valueToStore = value();
     }
 
-    await this.put(key, valueToStore, seconds);
+    await this.set(key, valueToStore, seconds);
 
     return valueToStore;
   }
-}
 
-export { RedisCacheModel };
+  private async createRedisClient(): Promise<Redis> {
+    return new Redis(this.options);
+  }
+}
