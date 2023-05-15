c63943580898a97eec7aa23161374bd9db580388
diaszano <luucas.dsantos@gmail.com>
2023-05-15 17:42:59 -0300

    refactor/classCache: Removidos

--- test/cache.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it, vi } from 'vitest';
-
-vi.mock('~/src/index');
-describe('cache', async (): Promise<void> => {
-  it('should sum two numbers', async (): Promise<void> => {
-    expect(1 + 1).toBe(2);
-  });
-});
